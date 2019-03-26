e5c21fc69699df4f01b77a25f82fa6c34c0831b7
UmurerwaDivine <divinekagame@gmail.com>
2019-03-26 23:17:07 +0200

    created registration file

--- insta/urls.py
+++ insta/urls.py
@@ -6,8 +6,8 @@ urlpatterns=[
     url('^$',views.index,name = 'welcome'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^new/profile$', views.new_profile, name='new_profile'),
-    url(r'^profile/',views.image,name='profile'),
-    # url(r'new/post$',views.posts,name='posts')
+    url(r'^profile/',views.image,name='posts'),
+    url(r'new/post$',views.posts,name='image')
  
 ]      
 if settings.DEBUG:
